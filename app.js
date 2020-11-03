371b2ccf1b5178959480a2a4e688b3014194b910
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-02 18:38:49 -0800

    adjusted database

--- app.js
+++ app.js
@@ -8,177 +8,215 @@ const cors = require('cors');
 app.get('/', (req, res) => {
   res.json([
       {
-        alias: 'Anarky',
-        name: 'Lonnie Machin'
-      },
-      {
-          alias: 'Bane',
-          name: 'Dorrance'
+        alias: 'Bane',
+        name: 'Edward',
+        alive: true,
+        category: 'Central Rogue'
       },
       {
         alias: 'Black Mask',
-        name: 'Roman Sionis'
+        name: 'Roman Sionis',
+        alive: false,
+        category: 'Central Rogues'
       },
       {
-        alias: 'Bronze Tiger',
-        name: 'Benjamin Turner'
-      },
-      {
-        alias: 'Catman',
-        name: 'Thomas Blake'
+        alias: 'The Boss',
+        name: 'Sal Maroni',
+        alive: false,
+        category: 'Organized Crime'
       },
       {
         alias: 'Catwoman',
-        name: 'Selina Kyle'
+        name: 'Selina Kyle',
+        alive: true,
+        category: 'Central Rogues'
       },
       {
         alias: 'Calendar Man',
-        name: 'Julian Gregory Day'
+        name: 'Julian Gregory Day',
+        alive: true,
+        category: 'Secondary Rogues'
       },
       {
         alias: 'Clayface',
-        name: 'Basil Karlo' 
-      },
-      {
-        alias: 'Clayface (II)',
-        name: 'Matthew "Matt" Haggen'
-      },
-      {
-        alias: 'Cluemaster',
-        name: 'Arthur Brown'
-      },
-      {
-        alias: 'Deathstroke',
-        name: 'Slade Wilson'
-      },
-      {
-        alias: 'Firefly',
-        name: 'Garfield Lynns'
-      },
-      {
-        alias: 'Great White Shark',
-        name: 'Warren White'
-      },
-      {
-        alias: 'Hugo Strange',
-        name: 'Professor Hugo Strange'
-      },
-      {
-        alias: 'Hush',
-        name: 'Dr. Thomas Elliot'
-      },
-      {
-        alias: 'J.J. Gordon',
-        name: 'James Gordon, Jr.'
-      },
-      {
-        alias: 'The Joker',
-        name: 'Jack Napier'
-      },
-      {
-        alias: 'Joker\'s Daughter',
-        name: 'Duela Dent'
-      },
-      {
-        alias: 'Killer Croc',
-        name: 'Waylon Jones'
-      },
-      {
-        alias: 'Killer Moth',
-        name: 'Drury Walker'
-      },
-      {
-        alias: 'Lady Shiva',
-        name: 'Sandra Woosan'
-      },
-      {
-        alias: 'Lincoln March',
-        name: 'Thomas Wayne, Jr.'
-      },
-      {
-        alias: 'Mad Hatter',
-        name: 'Dr. Jervis Tetch'
-      },
-      {
-        alias: 'Mr. Freeze',
-        name: 'Dr. Victor Fries'
-      },
-      {
-        alias: 'The Penguin',
-        name: 'Oswald Chesterfield Cobblepot'
-      },
-      {
-        alias: 'Poison Ivy',
-        name: 'Dr. Pamela Lillian Isley'
-      },
-      {
-        alias: 'Ra\'s al Ghul',
-        name: 'Ra\'s al Ghul'
-      },
-      {
-        alias: 'The Red Hood',
-        name: 'Jason Todd'
-      },
-      {
-        alias: 'The Riddler',
-        name: 'Edward Nigma/Nashton'
-      },
-      {
-        alias: 'The Scarecrow',
-        name: 'Dr. Jonathan Crane'
-      },
-      {
-        alias: 'Two-Face',
-        name: 'Harvey Dent'
-      },
-      {
-        alias: 'Man-Bat',
-        name: 'Dr. Kirk Langstrom'
-      },
-      {
-        alias: 'Maxie Zeus',
-        name: 'Maximillian Zeus'
-      },
-      {
-        alias: 'Mister Zsasz',
-        name: 'Victor Zsasz'
+        name: 'Basil Karlo',
+        alive: false,
+        category: 'Central Rogues'
       },
       {
         alias: 'Deadshot',
-        name: 'Floyd Lawton'
+        name: 'Floyd Lawton',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Deathstroke',
+        name: 'Slade Wilson',
+        alive: false,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Firefly',
+        name: 'Garfield Lynns',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Great White Shark',
+        name: 'Warren White',
+        alive: true,
+        category: 'Secondary Rogues'
+      },
+      {
+        alias: 'Hangman',
+        name: 'Sofia Gigante',
+        alive: false,
+        category: 'Organized Crime'
       },
       {
         alias: 'Harley Quinn',
-        name: 'Dr. Harleen F. Quinzel'
+        name: 'Dr. Harleen F. Quinzel',
+        alive: true,
+        category: 'Central Rogues'
       },
       {
-        alias: 'The Phantasm',
-        name: 'Andrea Beaumont'
+        alias: 'Holiday',
+        name: 'Alberto Falcone',
+        alive: false,
+        category: 'Organized Crime'
+      },
+      {
+        alias: 'Hugo Strange',
+        name: 'Professor Hugo Strange',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Hush',
+        name: 'Dr. Thomas Elliot',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'The Joker',
+        name: 'Jack Napier',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Killer Croc',
+        name: 'Waylon Jones',
+        alive: true,
+        category: 'Central ROgues'
+      },
+      {
+        alias: 'Mad Hatter',
+        name: 'Dr. Jervis Tetch',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Mr. Freeze',
+        name: 'Dr. Victor Fries',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Onomatopoeia',
+        name: 'N/A',
+        alive: true,
+        category: 'Recurring Rogues'
+      },
+      {
+        alias: 'The Penguin',
+        name: 'Oswald Chesterfield Cobblepot',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Poison Ivy',
+        name: 'Dr. Pamela Lillian Isley',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Ra\'s al Ghul',
+        name: 'Ra\'s al Ghul',
+        alive: true,
+        category: 'League of ASsassins'
+      },
+      {
+        alias: 'The Red Hood',
+        name: 'Jason Todd',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'The Riddler',
+        name: 'Edward Nigma/Nashton',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'The Roman',
+        name: 'Carmine Falcone',
+        alive: false,
+        category: 'Organized Crime'
+      },
+      {
+        alias: 'Two-Face',
+        name: 'Harvey Dent',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Man-Bat',
+        name: 'Dr. Kirk Langstrom',
+        alive: true,
+        category: 'Central Rogues'
+      },
+      {
+        alias: 'Maxie Zeus',
+        name: 'Maximillian Zeus',
+        alive: true,
+        category: 'Secondary Rogues'
+      },
+      {
+        alias: 'Mr. Zsasz',
+        name: 'Victor Zsasz',
+        alive: true,
+        category: 'Central Rogues'
       },
       {
         alias: 'Professor Pyg',
-        name: 'Dr. Lazlo Valentin'
+        name: 'Dr. Lazlo Valentin',
+        alive: true,
+        category: 'Secondary Rogues'
       },
       {
-        alias: 'Prometheus',
-        name: 'N/A'
+        alias: 'Scarecrow',
+        name: 'Professor Jonathan Crane',
+        alive: true,
+        category: 'Central Rogues'
       },
       {
         alias: 'Solomon Grundy',
-        name: 'Cyrus Gold'
+        name: 'Cyrus Gold',
+        alive: false,
+        category: 'Secondary Rogue'
       },
       {
         alias: 'Talia al Ghul',
-        name: 'Talia al Ghul'
+        name: 'Talia al Ghul',
+        alive: true,
+        category: 'League of Assassins'
       },
       {
-        alias: 'Talon',
-        name: 'William Cobb'
+        alias: 'Ventriloquist',
+        name: 'Arnold Wesker',
+        alive: true,
+        category: 'Central Rogues'
       },
-      {
-        alias: 'The Ventriloquist & Scarface',
-        name: 'Arnold Wesker'
-      }
   ])
 })
 
